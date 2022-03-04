0a2b96259acfd7ac0d1e2225520d2440a0a55b19
mattes <mathias.he@gmx.net>
2022-03-04 16:48:12 +0100

    Add current state of training

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+
+// bring in routes
+const postRoutes = require('./routes/post');
+
+app.get('/', postRoutes.getPosts);
+
+const port = 8080
+
+app.listen(port, () => {
+    console.log(`A Node Js API is listening on port: ${port}`)
+});
