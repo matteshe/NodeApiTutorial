355c35c678589f472aa1e9df561cd1e6722b9f1d
mattes <mathias.he@gmx.net>
2022-03-04 16:58:57 +0100

    Add own middleware just for test

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const morgan = require('morgan')
 // bring in routes
 const {getPosts} = require('./routes/post');
 
+const myOwnMiddleware = (req, res, next) => { 
+    console.log("middleware applied")
+
+    // important to move to the next phase, otherwise the app stuck
+    next();
+};
+
 // middleware
 app.use(morgan('dev'));
+app.use(myOwnMiddleware);
 
 app.get('/', getPosts);
 
