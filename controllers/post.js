279a7da5fdd1f3cbaf1bf7f608f61720697ec136
mattes <mathias.he@gmx.net>
2022-03-14 12:53:04 +0100

    Change log to real save of post in db

--- controllers/post.js
+++ controllers/post.js
@@ -10,5 +10,15 @@ exports.getPosts = (req, res) => {
 
 exports.createPost = (req, res) => {
     const post = new Post(req.body);
-    console.log("Creating Post: ", post);
+    //console.log("Creating Post: ", post);
+    post.save((err, result) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+        res.status(201).json({
+            message: "new post created."
+        });
+    })
 }
