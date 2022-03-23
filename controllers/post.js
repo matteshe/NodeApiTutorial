8c11295492ab0fb4cc45685721af4464c0796227
mattes <mathias.he@gmx.net>
2022-03-23 15:21:20 +0100

    Refactor remove SC 200 and json key data

--- controllers/post.js
+++ controllers/post.js
@@ -3,7 +3,7 @@ const Post = require('../models/post')
 exports.getPosts = (req, res) => {
     const posts = Post.find()
     .then((posts) => {
-        res.status(200).json({data: posts})
+        res.json({posts})
     })
     .catch(err => console.log(err))
 };
