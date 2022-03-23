8e3aadb1a9e1138786abe6fb494105f74cfe8823
mattes <mathias.he@gmx.net>
2022-03-23 15:19:39 +0100

    Add initial code to receive posts from DB

--- controllers/post.js
+++ controllers/post.js
@@ -1,11 +1,11 @@
 const Post = require('../models/post')
 
 exports.getPosts = (req, res) => {
-    res.json({posts: [
-        {title: "First post"},
-        {title: "Second post"}
-        ]   
-    });
+    const posts = Post.find()
+    .then((posts) => {
+        res.status(200).json({data: posts})
+    })
+    .catch(err => console.log(err))
 };
 
 exports.createPost = (req, res) => {
