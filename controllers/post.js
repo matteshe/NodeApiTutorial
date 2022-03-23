98d5f57f2af48fb2156b0689116a63005f0055f4
mattes <mathias.he@gmx.net>
2022-03-23 15:22:24 +0100

    Add select on interested db fields

--- controllers/post.js
+++ controllers/post.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post')
 
 exports.getPosts = (req, res) => {
-    const posts = Post.find()
+    const posts = Post.find().select("_id title body")
     .then((posts) => {
         res.json({posts})
     })
